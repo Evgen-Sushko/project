43a60176a9eab125d33032aaa6bcc9767cc595fa
Evgen <sunik@ukr.net>
2021-01-15 18:29:15 +0200

    prkticheskoe 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 "use strict";
 
+const namberOfFilms = +prompt("Skolko filmov vi posmotreli?", "0");
+//console.log(namberOfFilms);
 
-const a = true,
-    b = false; 
+const personalMovieDB = {
+	count: namberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+//console.log(personalMovieDB.privat);
+
+const a = prompt("kakoi posledniy film?", ""),
+	b = prompt("kakaya ocena?", "5"),
+	c = prompt("kakoi posledniy film?", ""),
+	d = prompt("kakaya ocena?", "5");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
