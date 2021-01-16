2a0e6ceae040d4eaf5a6bebfbd50730c1d14159b
Evgen <sunik@ukr.net>
2021-01-16 19:03:18 +0200

    prkticheskoe 2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,40 @@
 "use strict";
 
-if (4 == 4) {
-	console.log("ok!");
-} else {
-	console.log("error!!!");
+const namberOfFilms = +prompt("Skolko filmov vi posmotreli?", "0");
+//console.log(namberOfFilms);
+
+//const a = true,
+//b = false;  
+
+const personalMovieDB = {
+	count: namberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+//console.log(personalMovieDB.privat);
+
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt("kakoi posledniy film?", ""),
+	b = prompt("kakaya ocena?", "5");
+	personalMovieDB.movies[a] = b;
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		console.log("done");
+	} else {
+		console.log("error");
+		i--;
+	}
+	if (personalMovieDB.count < 10) {
+		console.log("Просмотрено довольно мало фильмов");
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log("Вы классический зритель");
+	}else if (personalMovieDB.count >= 30) {
+		console.log("Вы киноман");
+	} else {
+		console.log("Произошла ошибка");
+	}
 }
 
-let num = 40;
-(num == 50) ? console.log('ok') : console.log('error');
-
-const numm = 48;
-switch (numm) {
-	case 49: console.log("brihnya");
-		break;
-	case 50: console.log("v tochku");
-		break;
-	default: console.log("ne poluchilos");
-}
+console.log(personalMovieDB);
