75a627ab2d80ed4305cf50d67c3ffa64b404dc2f
Evgen <sunik@ukr.net>
2021-01-21 13:37:16 +0200

    praktika3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,63 @@
-"use strict";
+'use strict';
 
-let str = "tEst";
-console.log(str.length);
-let arr = [1, 2, 3];
-console.log(arr.length);
-console.log(str[2]);
-console.log(str.toLowerCase());
+let numberOfFilms;
 
-let fruit = 'some fruit';
-console.log(fruit.indexOf("f"));
-let slise = 'hello world';
-console.log(slise.substr(6, 2));
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+		while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+	}
+} 
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+    	const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    	} else {
+        console.log('error');
+        i--;
+    	}
+	}	
+}
+rememberMyFilms();
+
+function detectPersonLevel() {
+	if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+	} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+	} else {
+    console.log("Произошла ошибка");
+	}
+
+}
+detectPersonLevel();
+
+function showMyDB() {
+	if (personalMovieDB.privat ==false) {
+		console.log(personalMovieDB);
+	}
+}
+showMyDB();
+
+function writeYourGenres() {
+	for (let i = 1; i < 4; i++) {
+		personalMovieDB.genres [i-1]= prompt('Ваш любимый жанр под номером ' + i, '');
+	
+	}
+}
+	writeYourGenres();
---
+++ js/script2.js
@@ -0,0 +1,9 @@
+'use strict';
+let age = prompt("Сколько вам лет?");
+let old = prompt("С вами идет взрослый?");
+
+if (age >= 13 || old == "да") {
+    alert('Проходите')
+} else {
+    alert("Пшел отсюда пиздюк");
+}
